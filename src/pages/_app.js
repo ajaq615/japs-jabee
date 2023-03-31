44cd230ed63e1aebe81d2ba87a5f775372394d75
ajquidasol615 <ajquidasol615@gmail.com>
2023-03-31 16:56:54 +0800

    ..

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,9 @@ export default function App({ Component, pageProps }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Component {...pageProps} />
+      <div className='container'>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import MenuList from '../../components/MenuList'
 
 const Home = (props) => {
   const isEmpty = JSON.stringify(props.menu) === '{}';
-  console.log(isEmpty);
   return (
     <>
       <Head>
@@ -14,7 +13,16 @@ const Home = (props) => {
           content='Jabillee langhap sarap meals'
         ></meta>
       </Head>
-      <h1>Home Page</h1>
+      <div>
+      <img src='https://jb-ph-cdn.tillster.com/prod/Carousel/Project%20Panama%20Desktop%20Banner_Justin%20Vicente%20Matha.jpg_04a62d40-097f-4e2a-9b1e-87477818ecaa.jpg' />
+      </div>
+      <div className='menu_items'>
+      <img height="240" src='https://jb-ph-cdn.tillster.com/prod/Carousel/FA-JB_PH_INCREASE_SPOTLIGHT_TILE_792x240_opt.jpg_c6a6a29b-bdcd-4980-9f29-bc7e92977b20.jpg'></img>
+      <img height="240" src='https://jb-ph-cdn.tillster.com/qa/Carousel/JB_PH_INCREASE_FAMILY_MEALS_TILE_FA_736x490_opt.jpg_0f513f1c-9b7a-49ee-8c66-f4ddf0e06266.jpg'></img>
+      <img height="240" src='https://jb-ph-cdn.tillster.com/prod/Carousel/FA-JB_PH_INCREASE_CHICKENJOY_TILE_384x240_opt.jpg_cf81a220-9c12-4ffa-ac62-cd26708fe922.jpg'></img>
+      <img height="240" src='https://jb-ph-cdn.tillster.com/qa/Carousel/JB_PH_INCREASE_BURGER_TILE_FA_736x490_opt.jpg_0bca19df-8933-445c-8b2b-9a8a1f23d796.jpg'></img>
+      <img height="240" src='https://jb-ph-cdn.tillster.com/prod/Carousel/JBPH_THUMBNAIL_JANUARY-UPDATE_736x490px_FA_opt.jpg_d3b57903-46d8-405b-b755-5dede7d263ab.jpg'></img>
+      </div>
       {isEmpty ? (
         <h2>No menu found!</h2>
       ) : (
