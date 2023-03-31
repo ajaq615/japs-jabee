ad2c851dc5726d9eeb965b4f3cf28ce469b243e6
ajquidasol615 <ajquidasol615@gmail.com>
2023-03-31 11:55:41 +0800

    ..

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
+}
+
+export async function getMenu() {
+  try {
+    let response = await fetch('https://api-jollibee-menu.vercel.app/menu');
+    let data = await response.json();
+    return [data, null];
+  } catch (err) {
+    return [null, err];
+  }
 }
